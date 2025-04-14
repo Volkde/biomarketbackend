d4c102a1b8653f4282373330a1fc7513ad82c3f6
s-saratov <saratovs@yandex.ru>
2025-04-14 17:34:09 +0200

    classes WishlistMapper, WishlistResponse, WishlistService and WishlistServiceImpl add

---
+++ src/main/java/de/aittr/bio_marketplace/service/interfaces/WishlistService.java
@@ -0,0 +1,9 @@
+package de.aittr.bio_marketplace.service.interfaces;
+
+import de.aittr.bio_marketplace.domain.dto.WishlistDto;
+
+public interface WishlistService {
+
+    // --- Create ---
+    WishlistDto toggleProduct(Long userId, Long productId);
+}
